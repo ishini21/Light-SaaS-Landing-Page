e38126c017e818c52cbbc8dd37daff4df01a7067
ishini shavindhya <ishinishavindhya@gmail.com>
2025-06-26 20:17:05 +0530

    Updated ProductShowcase.tsx

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -17,8 +17,9 @@ export const ProductShowcase = () => {
    const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
   return (
     <section  ref={sectionRef} className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
-      <div className=".section-heading">
+     
       <div className="container">
+         <div className="section-heading">
         <div className="flex justify-center">
           <div className="tag">Boost your productivity</div>
         </div>
@@ -30,11 +31,11 @@ export const ProductShowcase = () => {
         </div>
         <div className="relative">
         <Image src={productImage} alt="product image" className="mt-10" />
-         <motion.img  src={pyramidImage.src} alt="pyramid image" height={262} width={262} className="hidden md:block absolute -right-30 -top-32" 
+         <motion.img  src={pyramidImage.src} alt="pyramid image" height={262} width={262} className="hidden md:block absolute -right-36 -top-32" 
          style={{
           translateY,
          }}/>
-        <motion.img  src={tubeImage.src} alt="tube image" height={248} width={248} className="hidden md:block absolute bottom-24 -left-25" 
+        <motion.img  src={tubeImage.src} alt="tube image" height={248} width={248} className="hidden md:block absolute bottom-24 -left-36" 
          style={{
           translateY,
          }}/>
